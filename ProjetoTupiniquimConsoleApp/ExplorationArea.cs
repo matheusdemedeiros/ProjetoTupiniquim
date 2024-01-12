a4f58ea5e31946f3ae0191907ea1e846c4bded47
Matheus de Medeiros <matheus.medeiros@ndd.tech>
2024-01-12 17:45:06 -0300

    refactor: melhora a classe explorationArea

--- ProjetoTupiniquimConsoleApp/ExplorationArea.cs
+++ ProjetoTupiniquimConsoleApp/ExplorationArea.cs
@@ -8,5 +8,34 @@ namespace ProjetoTupiniquim.ConsoleApp
         public int LimitY { get; set; }
         public List<Robot> Robots { get; set; }
 
+        public ExplorationArea()
+        {
+            Robots = new List<Robot>();
+        }
+
+        public void AddRobots(List<Robot> robots)
+        {
+            for (int i = 0; i < robots.Count; i++)
+            {
+                AddRobot(robots[i]);
+            }
+        }
+
+        public void AddRobot(Robot robot)
+        {
+            if (robot == null) { return; }
+
+            if (robot.PositionX > LimitX) { return; }
+
+            if (robot.PositionY > LimitY) { return; }
+
+            Robots.Add(robot);
+        }
+
+         public void CheckRobotMoviment(Robot robot, Moviment moviment)
+        {
+
+        }
+
     }
 }
